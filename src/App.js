40a3a468ed7d1f0af20a4efdf484c7c72f973c12
Rebecca Song <rebecca.song@outlook.com>
2018-07-29 21:46:15 -0400

    Modify App.js to include redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import "./App.css";
 
@@ -11,30 +12,73 @@ import PageTitle from "./components/PageTitle.js";
 import Event from "./components/Event.js";
 import Criteria from "./components/Criteria.js";
 
+import { getAllConferences } from "./actions/index";
+
 class App extends Component {
   componentDidMount() {
     // this is here as an example for how to connect to the backend
     // it should be removed once development has started
     this.props.healthCheck();
+    this.props.getData();
   }
-
+  
   render() {
+    
+    const { isLoading, conferences } = this.props;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div>
         <AppHeader>
           <AppTitle name="Call for Diversity" />
           <Button name="Submit Event" />
         </AppHeader>
-        <EventsContainer>
-          <PageTitle name="Upcoming Events" />
-          <Event>
-            <Criteria>
-            </Criteria>
-          </Event>
-        </EventsContainer>
+        <EventsComponentContainer />
       </div>
     );
   }
 }
 
-export default App;
+const EventsComponent = ({ conferences }) => (
+  <EventsContainer>
+    <PageTitle name="Upcoming Events" />
+    {
+      conferences.map(conf => 
+        <Event key={conf.event.id}>
+          <Criteria>
+            {conf.event.name}
+          </Criteria>
+        </Event>
+      )
+    }
+  </EventsContainer>
+);
+
+const mapStateToProps = state => {
+
+  const { conferences, isLoading } = state;
+
+  return {
+    conferences,
+    isLoading
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getData: () => dispatch(getAllConferences)
+  }
+}
+
+const connectEventsToStore = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+const EventsComponentContainer = connectEventsToStore(EventsComponent);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default App;
